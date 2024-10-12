96222f76d1cca96f3a4a851d48ba5a354218b8d7
Víctor Reyes <unhill@gmail.com>
2024-10-12 21:30:27 +0200

    Add function that fetches and logs response from the backend

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -8,4 +8,11 @@ form.addEventListener("submit", (event) => {
   const description = todoDescription.value;
   const todo = { title, description };
   console.log(todo);
+
+  getData();
 });
+
+async function getData() {
+  const responce = await fetch("http://localhost:4444");
+  console.log(await responce.text());
+}
