27f11457b6fb1ffa3007201efbf2f37f8367f1d6
Víctor Reyes <unhill@gmail.com>
2024-10-12 19:44:52 +0200

    Log form data

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Javascript working");
+const form = document.getElementById("form") as HTMLFormElement;
+const todoTitle = document.getElementById("title") as HTMLFormElement;
+const todoDescription = document.getElementById("description") as HTMLFormElement;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = todoTitle.value;
+  const description = todoDescription.value;
+  const todo = { title, description };
+  console.log(todo);
+});
