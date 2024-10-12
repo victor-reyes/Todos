e676dbd1bf4f07b70f7b8a71456448e592db747d
Víctor Reyes <unhill@gmail.com>
2024-10-12 21:29:35 +0200

    Allow any origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,9 @@ const port = 4444;
 const host = "localhost";
 
 const server = http.createServer(async (req, res) => {
-  res.statusCode = 200;
+  res.writeHead(200, {
+    "access-control-allow-origin": "*",
+  });
   res.end("Backend is running!");
 });
 
