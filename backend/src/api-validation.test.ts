4cfb6858df6c1e28e4ebfe81da27e37d936bb338
Víctor Reyes <unhill@gmail.com>
2024-10-13 15:48:19 +0200

    Add test for validating body as json

--- backend/src/api-validation.test.ts
+++ backend/src/api-validation.test.ts
@@ -14,3 +14,13 @@ describe("Route", () => {
     assert.deepStrictEqual(validateRoute("/api/v1/todo"), true);
   });
 });
+
+describe("Body", () => {
+  test("should be a of type Todo", () => {
+    let bodyJson = JSON.stringify({ title: "Assigment", description: "Be done by Saturday" });
+    let wrongBodyJson = JSON.stringify({ title: "Pride and Prejudice", author: "Jane Austen" });
+
+    assert.deepStrictEqual(validateBody(bodyJson), true);
+    assert.deepStrictEqual(validateBody(wrongBodyJson), false);
+  });
+});
