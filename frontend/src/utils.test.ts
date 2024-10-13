9eff9d5172b1337f46fa6e6caf0dd0cad020c57c
Víctor Reyes <unhill@gmail.com>
2024-10-13 20:54:48 +0200

    Include button in li element test

--- frontend/src/utils.test.ts
+++ frontend/src/utils.test.ts
@@ -15,25 +15,33 @@ describe("Utils", () => {
     const todo = { title: "title" };
 
     const li: HTMLLIElement = createListElement(todo);
-    const title = li.firstChild as Element;
+    const title = li.children[0];
+    const deleteButton = li.children[1];
 
-    assert.strictEqual(li.children.length, 1);
+    assert.strictEqual(li.children.length, 2);
     assert.strictEqual(title.tagName, "B");
     assert.strictEqual(title.textContent, "title");
+
+    assert.strictEqual(deleteButton.tagName, "BUTTON");
+    assert.strictEqual(deleteButton.textContent, "Delete");
   });
 
   it("should return li with bold title and description", () => {
     const todo = { title: "title", description: "description" };
 
     const li: HTMLLIElement = createListElement(todo);
-    const title = li.firstChild as Element;
-    const description = li.children[1] as Element;
+    const title = li.children[0];
+    const description = li.children[1];
+    const deleteButton = li.children[2];
 
-    assert.strictEqual(li.children.length, 2);
+    assert.strictEqual(li.children.length, 3);
     assert.strictEqual(title.tagName, "B");
     assert.strictEqual(title.textContent, "title");
 
     assert.strictEqual(description.tagName, "SPAN");
     assert.strictEqual(description.textContent, ": description");
+
+    assert.strictEqual(deleteButton.tagName, "BUTTON");
+    assert.strictEqual(deleteButton.textContent, "Delete");
   });
 });
