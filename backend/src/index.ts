43a3f2352f68b371b08bb66407d3cbdc45b28095
Víctor Reyes <unhill@gmail.com>
2024-10-13 12:35:36 +0200

    Check, if there is content (by loocking att content-length header) and return 200 if there is, or 400 otherwise

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,23 @@ const port = 4444;
 const host = "localhost";
 
 const server = http.createServer(async (req, res) => {
-  res.writeHead(200, {
-    "access-control-allow-origin": "*",
+  if (!req.headers["content-length"]) {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
   });
-  res.end(JSON.stringify(TODOS));
+  req.on("end", () => {
+    console.log(`body is ${body}`);
+    res.writeHead(200, {
+      "access-control-allow-origin": "*",
+    });
+    res.end(JSON.stringify(TODOS));
+  });
 });
 
 server.listen(port, host, () => {});
