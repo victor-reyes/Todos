750d574cc62de6bd06e1e76b7795d7d4a1f2eada
Víctor Reyes <unhill@gmail.com>
2024-10-13 11:57:12 +0200

    Add test for todo with both title and descripton

--- frontend/src/utils.test.ts
+++ frontend/src/utils.test.ts
@@ -21,4 +21,19 @@ describe("Utils", () => {
     assert.strictEqual(title.tagName, "B");
     assert.strictEqual(title.textContent, "title");
   });
+
+  it("should return li with bold title and description", () => {
+    const todo = { title: "title", description: "description" };
+
+    const li: HTMLLIElement = createListElement(todo);
+    const title = li.firstChild as Element;
+    const description = li.children[1] as Element;
+
+    assert.strictEqual(li.children.length, 2);
+    assert.strictEqual(title.tagName, "B");
+    assert.strictEqual(title.textContent, "title");
+
+    assert.strictEqual(description.tagName, "span");
+    assert.strictEqual(description.textContent, ": description");
+  });
 });
