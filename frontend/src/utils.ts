65623cd812b759694b9eecf29b82c47b5e6a3456
Víctor Reyes <unhill@gmail.com>
2024-10-13 21:01:39 +0200

    Add delete button

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -5,12 +5,17 @@ export function createListElement(todo: Todo) {
   const titleContainer = document.createElement("b");
   titleContainer.textContent = title;
   li.appendChild(titleContainer);
-  
+
   if (description) {
     const descriptionContainer = document.createElement("span");
     descriptionContainer.textContent = `: ${description}`;
     li.appendChild(descriptionContainer);
   }
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  li.appendChild(deleteBtn);
 
   return li;
 }
