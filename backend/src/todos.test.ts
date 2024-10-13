12f2735c75df39162618d4af4d491d908749dbfa
Víctor Reyes <unhill@gmail.com>
2024-10-13 19:41:35 +0200

    Create tests for validating add and remove todos operations

---
+++ backend/src/todos.test.ts
@@ -0,0 +1,31 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("Todos", () => {
+  it("Should add todo", () => {
+    const todos = [{ title: "title" }, { title: "title", description: "description" }];
+
+    const todoToAdd = { title: "title", description: "whatever" };
+
+    assert.deepStrictEqual(addTodo(todos, todoToAdd), [
+      { title: "title", description: "whatever" },
+      { title: "title" },
+      { title: "title", description: "description" },
+    ]);
+  });
+
+  it("Should add todo", () => {
+    const todos = [
+      { title: "title", description: "whatever" },
+      { title: "title" },
+      { title: "title", description: "description" },
+    ];
+
+    const todoToRemove = { title: "title", description: "whatever" };
+
+    assert.deepStrictEqual(removeTodo(todos, todoToRemove), [
+      { title: "title" },
+      { title: "title", description: "description" },
+    ]);
+  });
+});
